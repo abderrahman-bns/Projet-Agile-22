27f971fbd3cd3b4d3b9707058115d09245b5c956
Mohamed yahyaoui <mohamedyahyaoui2000smia@gmail.com>
2022-05-20 23:26:55 +0100

    Api gateway

---
+++ Api_Gateway/src/test/java/bank/app/api_gateway/Api_Gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.app.api_gateway.Api_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
