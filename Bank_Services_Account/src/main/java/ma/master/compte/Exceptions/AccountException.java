41239650bc6b15adc21238130dc00c8ec0e30316
oualyaymane <93880084+oualyaymane@users.noreply.github.com>
2022-05-22 21:11:31 +0100

    new start

--- Bank_Services_Account/src/main/java/ma/master/compte/Exceptions/AccountException.java
+++
@@ -1,9 +0,0 @@
-package ma.master.compte.Exceptions;
-
-public class AccountException extends RuntimeException{
-	
-	public AccountException(String message) {
-		super(message);
-	}
-
-}
--- Bank_Services_Account/src/main/java/ma/master/compte/Exceptions/RestTemplateException.java
+++
@@ -1,8 +0,0 @@
-package ma.master.compte.Exceptions;
-
-public class RestTemplateException extends RuntimeException{
-    public RestTemplateException(String message) {
-        super(message);
-    }
-
-}
