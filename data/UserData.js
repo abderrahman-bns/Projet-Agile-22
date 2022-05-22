0de98e9cce744d525173bfa9395109db51758ed7
BEN SALAH Abderrahman <40049722+abderrahman-bns@users.noreply.github.com>
2022-05-23 00:15:42 +0200

    BAE-63
    BAE-63 Fix Connection problem

--- data/UserData.js
+++ data/UserData.js
@@ -2,11 +2,11 @@ import userModel from "../models/userModel";
 
 export const USER = new userModel(
   "u1",
-  "Lamia Belhadi",
-  "Lamia@belhadi.com",
-  "06682565487",
+  "BEN SALAH Abderrahman",
+  "abderrahmanbns21@gmail.com",
+  "0689667680",
   "Agence Casablanca",
   ["a1", "a2"],
   "78451202154874",
-  "123456789"
+  "637386"
 );
