0f02f22c2272909198d442899b971f9a6e0ed133
oualyaymane <93880084+oualyaymane@users.noreply.github.com>
2022-05-22 22:15:22 +0100

    BAE-35
    BAE-35 Exception Account

---
+++ src/main/java/ma/master/compte/Exceptions/AccountException.java
@@ -0,0 +1,9 @@
+package ma.master.compte.Exceptions;
+
+public class AccountException extends RuntimeException{
+	
+	public AccountException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/main/java/ma/master/compte/Exceptions/RestTemplateException.java
@@ -0,0 +1,8 @@
+package ma.master.compte.Exceptions;
+
+public class RestTemplateException extends RuntimeException{
+    public RestTemplateException(String message) {
+        super(message);
+    }
+
+}
