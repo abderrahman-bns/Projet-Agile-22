5732472c2428f1fa98bbef1887adf6b7180116bd
khaoula-bouzraf <khaoula.bouzraf@um5r.ac.ma>
2022-05-22 22:04:55 +0100

    BAE-10
    BAE-10 Setup Service Card

---
+++ Bank_Services_Card/src/test/java/bank/app/card_service/CardServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.app.card_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
